f80e1d83d90c6df3f18bfb2f76ce134b8c89a51c
BraveKnight6750 <>
2026-07-01 17:42:56 -0500

    added quiver item

--- src/main/java/net/braveknight6/betterbows/BetterBowsDataGenerator.java
+++ src/main/java/net/braveknight6/betterbows/BetterBowsDataGenerator.java
@@ -1,11 +1,14 @@
 package net.braveknight6.betterbows;
 
+import net.braveknight6.betterbows.datagen.ModModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class BetterBowsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		var pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModModelProvider::new);
 	}
 }
---
+++ src/main/java/net/braveknight6/betterbows/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.braveknight6.betterbows.item;
+
+import net.braveknight6.betterbows.BetterBows;
+import net.fabricmc.fabric.api.creativetab.v1.CreativeModeTabEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+
+import java.util.function.Function;
+
+public class ModItems {
+    public static final Item QUIVER = registerItem("quiver", Item::new);
+
+    public static Item registerItem(String name, Function<Item.Properties, Item> function){
+        return Registry.register(BuiltInRegistries.ITEM, Identifier.fromNamespaceAndPath(BetterBows.MOD_ID, name),
+                function.apply(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(BetterBows.MOD_ID, name)))));
+    }
+
+    public static void registerModItems() {
+        BetterBows.LOGGER.info("Registering mod items for " + BetterBows.MOD_ID);
+
+        CreativeModeTabEvents.modifyOutputEvent(CreativeModeTabs.COMBAT).register(output -> {
+            output.accept(QUIVER);
+        });
+    }
+}
