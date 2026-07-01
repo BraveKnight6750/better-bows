8abad8b6b38318383326f342f00609655d7c8158
BraveKnight6750 <>
2026-07-01 16:56:13 -0500

    initial commit

---
+++ src/main/java/net/braveknight6/betterbows/BetterBowsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.braveknight6.betterbows;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterBowsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
